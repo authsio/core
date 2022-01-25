7b4eb7697fce5059ce2bf5b623857f043c04d5b5
Lundin Matthews <lundinmatthews@me.com>
2022-01-25 07:07:07 -0600

    Create project.resolver.ts

---
+++ backend/src/models/projects/project.resolver.ts
@@ -0,0 +1,7 @@
+import { Resolver } from "type-graphql";
+import { Project } from "./project.type";
+
+@Resolver(() => Project)
+export class ProjectResolver {
+  // TODO: Stub out all project crud
+}
