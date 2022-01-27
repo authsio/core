7e2ee81a0e3210614fc20d39e8495c3cc5dbb154
Lundin Matthews <lundinmatthews@me.com>
2022-01-27 08:04:17 -0600

    Thinking through the bootstrap new project, this will key to getting started

--- backend/src/models/users/user.input.ts
+++ backend/src/models/users/user.input.ts
@@ -1,4 +1,18 @@
-import { InputType } from "type-graphql";
+import { IsEmail } from "class-validator";
+import { Field, InputType } from "type-graphql";
 
 @InputType()
-export class BootstrapNewAccountInput {}
+export class BootstrapNewAccountInput {
+  @IsEmail()
+  @Field()
+  email!: string;
+
+  @Field()
+  password!: string;
+
+  @Field({ nullable: true })
+  firstName!: string;
+
+  @Field({ nullable: true })
+  lastName!: string;
+}
--- backend/src/models/users/user.resolver.ts
+++ backend/src/models/users/user.resolver.ts
@@ -1,5 +1,14 @@
-import { Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { Context } from "../..";
+import { BootstrapNewAccountInput } from "./user.input";
 import { User } from "./user.type";
+
+const doNotTouchSchemas = [
+  "pg_toast",
+  "pg_catalog",
+  "public",
+  "information_schema",
+];
 
 @Resolver(() => User)
 export class UserResolver {
@@ -7,7 +16,22 @@ export class UserResolver {
     description:
       "This can only be used to bootstrap the project and reject calls after the initial setup",
   })
-  async bootstrap(): Promise<boolean> {
+  async bootstrap(
+    @Arg("data") _input: BootstrapNewAccountInput,
+    @Ctx() { sequelize }: Context
+  ): Promise<boolean> {
+    const [results, metadata] = (await sequelize.query(
+      "SELECT schema_name FROM information_schema.schemata"
+    )) as [{ schema_name: string }[], any];
+    // Filter postgres and public out
+    const touchableSchemas = results.filter(
+      (i) => !doNotTouchSchemas.includes(i.schema_name)
+    );
+    if (touchableSchemas.length) {
+      return false;
+    }
+    // NOW WE HAVE ENSURED THAT NO OTHER SCHEMA ARE OUT THERE
+    // THIS MEANS WE CAN BOOTSTRAP A NEW PROJECT
     return false;
   }
 }
