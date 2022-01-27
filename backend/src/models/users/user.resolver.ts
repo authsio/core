5bad04a5f41fca43256ef24c510f0436eb859a1a
Lundin Matthews <lundinmatthews@me.com>
2022-01-27 08:23:45 -0600

    Thinking about this Logic

--- backend/src/models/users/user.resolver.ts
+++ backend/src/models/users/user.resolver.ts
@@ -2,6 +2,8 @@ import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { Context } from "../..";
 import { BootstrapNewAccountInput } from "./user.input";
 import { User } from "./user.type";
+import { v4 as uuidv4 } from "uuid";
+import { publicTables } from "../../libs/db";
 
 const doNotTouchSchemas = [
   "pg_toast",
@@ -32,6 +34,25 @@ export class UserResolver {
     }
     // NOW WE HAVE ENSURED THAT NO OTHER SCHEMA ARE OUT THERE
     // THIS MEANS WE CAN BOOTSTRAP A NEW PROJECT
+    // WE NEED TO FORCE THE FIRST SCHEMA UUID (name)
+    const schema = uuidv4();
+    try {
+      await sequelize.createSchema(schema, {});
+      // Postgres might not have this extension by default
+      await sequelize.query("CREATE EXTENSION IF NOT EXISTS citext;");
+      await sequelize.sync({ schema });
+      await Promise.all(
+        publicTables.map((dbModel) =>
+          sequelize.query(
+            `DROP TABLE IF EXISTS "${schema}"."${dbModel.getTableName()}";`
+          )
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    // CREATE LOGIN, CREATE USER, CREATE KEY PUBLIC & PRIVATE
+    // THIS IS THE BASE PROJECT THAT WILL ALLOW OTHER PROJECTS
     return false;
   }
 }
