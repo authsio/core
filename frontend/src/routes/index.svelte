58dc9e5de8abe14efb1344ecc855feeaccbc40ba
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-02-18 20:27:01 +0000

    dev: Set up tailwind + micro Svelte demo

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const message = `*** Authsio machine speaking. Do you copy? ***`;
+	const acknowledgement = "Let's goooooo!";
+
+	let acknowledged = false;
+	function acknowledge() {
+		acknowledged = true;
+	}
+</script>
+
+<div class="container px-6 py-10">
+	<h1 class="text-2xl font-bold text-left">
+		{message}
+	</h1>
+
+	<form class="py-5" on:submit|preventDefault={acknowledge}>
+		<fieldset>
+			<button
+				class="
+        bg-slate-300 text-slate-900 px-4 py-2 rounded
+        dark:bg-slate-900 dark:text-slate-100"
+				type="submit">I do copy!</button
+			>
+		</fieldset>
+	</form>
+
+	{#if acknowledged}
+		<p>{acknowledgement}</p>
+	{/if}
+</div>
